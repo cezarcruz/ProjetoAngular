afde08918b2216a5598a0c5587765e04100281f6
Cezar Cruz <cezarcruz@outlook.com>
2014-12-20 00:36:17 -0200

    Versão inicial com o Angular.JS e Spring MVC configurado

---
+++ src/main/java/br/com/cezarcruz/Application.java
@@ -0,0 +1,15 @@
+package br.com.cezarcruz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class Application {
+	public void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
---
+++ src/main/java/br/com/cezarcruz/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package br.com.cezarcruz.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	@RequestMapping("/oi")
+	public String oi() {
+		return "Oi mundo";
+	}
+}
