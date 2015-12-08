f65a7ff1c52119029399576098aebc6f4ee1208f
Cezar Cruz <cezarcruz@outlook.com>
2015-12-07 23:18:29 -0200

    Adding fake test.

---
+++ src/test/java/br/com/cezarcruz/data/models/SeriesRequestTest.java
@@ -0,0 +1,24 @@
+package br.com.cezarcruz.data.models;
+
+import br.com.cezarcruz.web.json.SeriesRequest;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Really Really fake test
+ */
+public class SeriesRequestTest {
+
+    @Test
+    public void fakeTest() {
+        SeriesRequest s = new SeriesRequest();
+        s.setId(1L);
+        s.setName("foo");
+        s.setSeason(1);
+
+        assertEquals(Long.valueOf(1), s.getId());
+        assertEquals("foo", s.getName());
+        assertEquals(Integer.valueOf(1), s.getSeason());
+    }
+}
