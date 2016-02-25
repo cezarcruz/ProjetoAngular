3da12de34d225e9c2ef289263caeb170a0f0d627
Cezar Cruz <cezarcruz@outlook.com>
2016-02-24 21:16:03 -0300

    Change version of Angular to 1.5

--- src/main/java/br/com/cezarcruz/data/repositories/CharacterRepository.java
+++ src/main/java/br/com/cezarcruz/data/repositories/CharacterRepository.java
@@ -6,9 +6,6 @@ import org.springframework.data.repository.Repository;
 
 import java.util.List;
 
-/**
- * Created by cezar on 28/04/15.
- */
 public interface CharacterRepository extends Repository<Character, Long> {
     Character save(final Character p);
     List<Character> findAll();
