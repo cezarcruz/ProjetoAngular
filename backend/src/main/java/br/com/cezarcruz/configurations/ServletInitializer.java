59b47c60a6b433530136a419512d0d7ee351690a
Cezar Cruz <cezarcruz@outlook.com>
2017-07-29 12:35:49 -0300

    updating project location

--- backend/src/main/java/br/com/cezarcruz/configurations/ServletInitializer.java
+++ backend/src/main/java/br/com/cezarcruz/configurations/ServletInitializer.java
@@ -2,7 +2,7 @@ package br.com.cezarcruz.configurations;
 
 import br.com.cezarcruz.Application;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * Initialize the spring web
